a517d0956e56b66133bc846f5d50c23cab62dbda
izaan <izaansaquib@gmail.com>
2023-03-09 18:50:22 +0500

    removeItem,increase,decrease actions added

--- src/feature/cart/cartSlice.js
+++ src/feature/cart/cartSlice.js
@@ -13,11 +13,45 @@ const cartSlice=createSlice({
     reducers:{
         clearCart:(state)=>{
             state.cartItems=[]
+            // return {cartItems:[]} will remove all intial state property values
         },
+        removeItem:(state,action)=>{
+            const itemId=action.payload;
+            // const itemId='recB6qcHPxb62YJ75';
+            // console.log(`payload : ${itemId}`)
+            // for(let i=0;i<state.amount;i++){
+            //     console.log(`state CartItems: ${state.cartItems[i].id}`)
+            // }
+            // console.log(`state ID: ${state.cartItems[0].id}`)
+            state.cartItems=state.cartItems.filter((item)=>item.id!==itemId)
+            // for(let i=0;i<state.amount;i++){
+            //     console.log(`state CartItems: ${state.cartItems[i].id}`)
+            // }
+                // state.cartItems.splice(state.cartItems.findIndex((arrow) => arrow.id === itemId), 1);
+            // console.log(`state Id: ${cartItem.id}`)//find function
+
+            //Explaination for line 26 'state.cartItems=state.cartItems.filter((item)=>item.id!==itemId)'
+            //state.cartItems is the cartItems property of the current state.
+            //filter is a method of arrays in JavaScript that creates a new array containing all elements that pass a test. In this case, the test is that the id property of each element in the array is not equal to itemId.
+            //The filter method returns a new array that contains all cart items except the one with the specified ID.
+            //The new array returned by filter is then assigned to state.cartItems, effectively replacing the old array with the updated one.
+        },
+        increase:(state,{payload})=>{
+            const cartItem=state.cartItems.find((item)=>
+            item.id===payload.id);
+
+            console.log(cartItem.id)
+            cartItem.amount=cartItem.amount + 1;
+        },
+        decrease:(state,{payload})=>{
+            const cartItem=state.cartItems.find((item)=>
+            item.id===payload.id);
+            cartItem.amount=cartItem.amount - 1;
+        }
     },
 })
 
 // console.log(cartSlice);
 
-export const {clearCart}=cartSlice.actions
+export const {clearCart,removeItem,increase,decrease}=cartSlice.actions
 export default cartSlice.reducer;
